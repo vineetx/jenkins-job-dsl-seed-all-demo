b57fd6c80b583903669e05115ce2b4440d1683cf
Vineet <vineetj_29@yahoo.com>
2018-04-27 13:04:18 +0530

    Update hello_world.groovy

--- jobs/hello_world.groovy
+++ jobs/hello_world.groovy
@@ -1,10 +1,4 @@
 job('Hello World') {
-  // This defines job parameters that are populated before job is run or default is used
-  parameters {
-        booleanParam('FLAG', true)
-        choiceParam('OPTION', ['option 1 (default)', 'option 2', 'option 3'])
-    }
-
   stages {
     stage("Hello World") {
       steps {
