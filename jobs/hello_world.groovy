67897cb7f9987ff100afc5e4c6c21d7e0d492bb9
Vineet <vineetj_29@yahoo.com>
2018-04-27 13:03:56 +0530

    Update hello_world.groovy

--- jobs/hello_world.groovy
+++ jobs/hello_world.groovy
@@ -5,12 +5,6 @@ job('Hello World') {
         choiceParam('OPTION', ['option 1 (default)', 'option 2', 'option 3'])
     }
 
-  // Triggers define how the job is triggered.
-  // Jobs may still be triggered manually or by webhook as well here.
-  triggers {
-    cron('@daily')
-  }
-
   stages {
     stage("Hello World") {
       steps {
