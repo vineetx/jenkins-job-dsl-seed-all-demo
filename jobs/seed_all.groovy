0af3fa44ca3555192294f4d522773a2610aa69ac
Vineet <vineetj_29@yahoo.com>
2018-04-27 12:57:17 +0530

    Update seed_all.groovy

--- jobs/seed_all.groovy
+++ jobs/seed_all.groovy
@@ -1,6 +1,6 @@
 job('Seed All') {
   scm {
-    git ('https://github.com/robinbowes/jenkins-job-dsl-seed-all-demo.git')
+    git ('https://github.com/vineetx/jenkins-job-dsl-seed-all-demo.git')
   }
   steps {
     dsl {
